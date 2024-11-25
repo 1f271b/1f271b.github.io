4b664b9f8c0aac2a72c578f9eab9ad6cbf694d1f
Jan <janvaglica@gmail.com>
2024-11-25 13:47:13 +0100

    Code cleanup from template

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,14 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <main>
-    <TheWelcome />
+    <div class="navbar bg-base-100">
+      <div class="flex-1">
+        <a class="btn btn-ghost text-xl">daisyUI</a>
+      </div>
+      <div class="flex-none">
+        <button class="btn btn-square btn-ghost"></button>
+      </div>
+    </div>
   </main>
 </template>
